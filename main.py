fd1a3ded6dfa6d804e0a0068870db63ad5231ee3
Alejandro Briceño <terremoto_alejandro@hotmail.com>
2026-07-24 23:38:28 -0400

    Finalizado landing,inicio y registro

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ async def landing():
 async def login():
     return FileResponse("frontend/views/login.html", media_type="text/html")
 
+@app.get("/register")
+async def register():
+    return FileResponse("frontend/views/login.html", media_type="text/html")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
